4cdf3fff2e27a19a5333aac0f16f80dfde23e79b
Anton Molchanov <mo4anov.a@gmail.com>
2023-06-17 16:50:04 +0300

    fix endpoint users/me

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '84.201.136.174', 'backend']
 
